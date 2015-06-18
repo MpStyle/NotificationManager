d3558d04ff44fb43f026c8f2d80a65255b3a8476
michele.pagnin <michele.pagnin@PENELOPE.ptqs.it>
2015-06-18 12:54:14 +0200

    Cambiate molte cose per fare andare i web service. FIX.

--- Web/WebServices/Version_1_0_0/AbstractWebService.php
+++ Web/WebServices/Version_1_0_0/AbstractWebService.php
@@ -6,110 +6,112 @@ use MToolkit\Controller\MAbstractHttpHandler;
 
 abstract class AbstractWebService extends MAbstractHttpHandler
 {
-	private $result=true;
-	private $resultDescription=null;
-	private $requestId=null;
-	private $webServiceName=null;
-	private $responseKey=null;
-	private $responseValue=null;
-	
-	public function run()
+
+    private $result = true;
+    private $resultDescription = null;
+    private $requestId = null;
+    private $webServiceName = null;
+    private $responseKey = null;
+    private $responseValue = null;
+
+    public function run()
     {
-		$this->requestId = $this->getPost()->getValue("requestId");
-		
-		$this->exec();
-		
-		$responseArray=array(
-			"WebService" => $this->webServiceName
-			, "ResponseId" => $this->requestId
-			, "Result" => $this->result
-			, "ResultDescription" => $this->resultDescription
-		);
-		
-		// Aggiunge la response custom del servizio se settata
-		if( $this->responseKey!=null )
-		{
-			$responseArray[$this->responseKey]=$this->responseValue;
-		}
-		
-		$this->setOutput(json_encode($responseArray));
-	}
-	
-	public function setResponse( $key, array $value )
-	{
-		MDataType::mustBeString($key);
-		
-		$this->responseKey=$key;
-		$this->responseValue=$value;
-	}
-	
-	public abstract function exec();
-        
-        public function getResult()
+        $this->requestId = $this->getPost()->getValue("requestId");
+
+        $this->exec();
+
+        $responseArray = array(
+            "WebService" => $this->webServiceName
+            , "ResponseId" => $this->requestId
+            , "Result" => $this->result
+            , "ResultDescription" => $this->resultDescription
+        );
+
+        // Aggiunge la response custom del servizio se settata
+        if ($this->responseKey != null)
         {
-            return $this->result;
+            $responseArray[$this->responseKey] = $this->responseValue;
         }
 
-        public function getResultDescription()
+        $this->setOutput(json_encode($responseArray));
+    }
+
+    public function setResponse($key, array $value)
+    {
+        MDataType::mustBeString($key);
+
+        $this->responseKey = $key;
+        $this->responseValue = $value;
+    }
+
+    public abstract function exec();
+
+    public function getResult()
+    {
+        return $this->result;
+    }
+
+    public function getResultDescription()
+    {
+        return $this->resultDescription;
+    }
+
+    public function getRequestId()
+    {
+        return $this->requestId;
+    }
+
+    public function getWebServiceName()
+    {
+        return $this->webServiceName;
+    }
+
+    public function setResult($result)
+    {
+        $this->result = $result;
+        return $this;
+    }
+
+    public function setResultDescription($resultDescription)
+    {
+        $this->resultDescription = $resultDescription;
+        return $this;
+    }
+
+    public function setRequestId($requestId)
+    {
+        $this->requestId = $requestId;
+        return $this;
+    }
+
+    public function setWebServiceName($webServiceName)
+    {
+        $this->webServiceName = $webServiceName;
+        return $this;
+    }
+
+    protected function objectToArray($obj)
+    {
+        if (is_object($obj))
         {
-            return $this->resultDescription;
+            $obj = (array) $obj;
         }
 
-        public function getRequestId()
+        if (is_array($obj))
         {
-            return $this->requestId;
+            $new = array();
+
+            foreach ($obj as $key => $val)
+            {
+                $new[$key] = $this->objectToArray($val);
+            }
+        }
+        else
+        {
+            $new = $obj;
         }
 
-        public function getWebServiceName()
-        {
-            return $this->webServiceName;
-        }
+        return $new;
+    }
 
-        public function setResult($result)
-        {
-            $this->result = $result;
-            return $this;
-        }
-
-        public function setResultDescription($resultDescription)
-        {
-            $this->resultDescription = $resultDescription;
-            return $this;
-        }
-
-        public function setRequestId($requestId)
-        {
-            $this->requestId = $requestId;
-            return $this;
-        }
-
-        public function setWebServiceName($webServiceName)
-        {
-            $this->webServiceName = $webServiceName;
-            return $this;
-        }
-
-	protected function objectToArray($obj)
-	{
-		if(is_object($obj))
-		{
-			$obj = (array) $obj;
-		}
-		
-		if(is_array($obj)) 
-		{
-			$new = array();
-			
-			foreach($obj as $key => $val) 
-			{
-				$new[$key] = $this->objectToArray($val);
-			}
-		}
-		else
-		{
-			$new = $obj;	
-		}
-		
-		return $new;       
-	}
 }
--- Web/WebServices/Version_1_0_0/GetNotifications.php
+++ Web/WebServices/Version_1_0_0/GetNotifications.php
@@ -2,31 +2,37 @@
 
 namespace Web\WebServices\Version_1_0_0;
 
-use MToolkit\Controller\MAbstractHttpHandler;
+use BusinessLogic\Application\Application;
+use BusinessLogic\Application\ApplicationBook;
+use BusinessLogic\Device\Device;
+use BusinessLogic\Device\DeviceBook;
+use MToolkit\Model\Sql\MDbConnection;
 
-class GetNotifications extends MAbstractHttpHandler
+class GetNotifications extends AbstractWebService
 {
+
     public function exec()
     {
         parent::setWebService(__CLASS__);
-		
+
         $mobileId = $this->getPost()->getValue("mobileId");
         $clientId = $this->getPost()->getValue("clientId");
-		
-		try
-		{
-			/* @var $device Device */ $device = DeviceBook::getDevices(null, null, null, null, null, $mobileId)->at(0);
-			/* @var $application Application */ $application = ApplicationBook::getApplications(null, null, $clientId)->at(0);
-			/* @var notifications MList */ $notifications = DeviceBook::getNotification($device->getId(), $application->getId());
 
-			parent::setResponse("Notifications", parent::objectToArray(notifications->__toArray()));
-		}
-		catch(OutOfBoundException $ex)
-		{
-			MDbConnection::getDbConnection()->rollBack();
-			
-			parent::setResult(false);
-			parent::setResultDescription("Device or application not found.");
-		}
+        try
+        {
+            /* @var $device Device */ $device = DeviceBook::getDevices(null, null, null, null, null, $mobileId)->at(0);
+            /* @var $application Application */ $application = ApplicationBook::getApplications(null, null, $clientId)->at(0);
+            /* @var notifications MList */ $notifications = DeviceBook::getNotification($device->getId(), $application->getId());
+
+            parent::setResponse("Notifications", parent::objectToArray($notifications->__toArray()));
+        }
+        catch (OutOfBoundException $ex)
+        {
+            MDbConnection::getDbConnection()->rollBack();
+
+            parent::setResult(false);
+            parent::setResultDescription("Device or application not found.");
+        }
     }
+
 }
