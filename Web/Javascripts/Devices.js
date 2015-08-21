bfdd86091d592dfba534f679831ed5894c582ee5
Michele Pagnin <agv46@tiscali.it>
2015-08-21 10:14:10 +0200

    Bug fix: ora è possibile rimuovere device.

--- Web/Javascripts/Devices.js
+++ Web/Javascripts/Devices.js
@@ -2,8 +2,8 @@ function Devices()
 {
     $('.DeleteDeviceModal').on('show.bs.modal', function (event) {
         var $button = $(event.relatedTarget);
-        $('.DeleteDeviceModal .MobileId').html($button.siblings(".MobileId").val());
-        $('.DeleteDeviceModal .DeleteDeviceId').val($button.siblings(".DeviceId").val());
+        $('.DeleteDeviceModal .MobileId').html($button.parents("form").find(".MobileId").val());
+        $('.DeleteDeviceModal .DeleteDeviceId').val($button.parents("form").find(".DeviceId").val());
     });
 
     $(".ErrorMessage").fadeTo(2000, 500).slideUp(500, function () {
