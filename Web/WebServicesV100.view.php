bfae784a7423f4333e7a27ac34d053b8bb70e445
Michele Pagnin <pagnin.michele@gmail.com>
2015-11-01 11:53:54 +0100

    Aggiunto servizio "GetregistrationStatus". Aggiunto il campo "notificationId" nel servizio "getNotifications"

--- Web/WebServicesV100.view.php
+++ Web/WebServicesV100.view.php
@@ -226,6 +226,13 @@ use BusinessLogic\Localization\LocalizationBook;
                                     <input type="text" name="clientId" class="form-control" />
                                 </div>
                             </div>
+                            
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">notificationId:</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="notificationId" class="form-control" placeholder="se valorizzato a null restituisce tutte le notifiche" />
+                                </div>
+                            </div>
 
                             <div class="form-group">
                                 <label class="col-sm-2 control-label">type:</label>
@@ -234,6 +241,83 @@ use BusinessLogic\Localization\LocalizationBook;
                                         <option></option>
                                         <?php foreach( DeviceBook::getDeviceType() as $value ): ?>
                                             <option value="<?php echo $value ?>"><?php echo $value ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                    <button type="submit" class="btn btn-primary">Send</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+            
+            
+            <div class="panel panel-primary">
+                <div class="panel-heading" role="tab" id="get_registration_status_title">
+                    <h3 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#web_services_accordion" href="#get_registration_status" aria-expanded="true" aria-controls="get_registration_status">
+                            Get registration status
+                        </a>
+                    </h3>
+                </div>
+                <div id="get_registration_status" class="panel-collapse collapse" role="tabpanel" aria-labelledby="get_registration_status_title">
+                    <div class="panel-body">
+                        <form method="post" action="WebServicesV100Response.php" class="form-horizontal">
+                            <input type="hidden" name="methodName" value="Get registration status" />
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Url:</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="methodUrl" value="<?php echo $this->getCurrentURL() ?>MobileRegistrationStatus.php" class="form-control" value="" readonly />
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">requestId:</label>
+                                <div class="col-sm-10">
+                                    <input type="number" name="requestId" class="form-control" value="" placeholder="A number to identify the request..." />
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">mobileId:</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="mobileId" class="form-control" value="" />
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">clientId:</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="clientId" class="form-control" />
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">type:</label>
+                                <div class="col-sm-10">
+                                    <select name="type" class="form-control" required>
+                                        <option></option>
+                                        <?php foreach( DeviceBook::getDeviceType() as $value ): ?>
+                                            <option value="<?php echo $value ?>"><?php echo $value ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">localizationId:</label>
+                                <div class="col-sm-10">
+                                    <select name="localizationId" class="form-control" required>
+                                        <option></option>
+                                        <?php foreach( LocalizationBook::getLocalizations() as $key => $value ): ?>
+                                            <option value="<?php echo $value ?>"><?php echo $key ?></option>
                                         <?php endforeach; ?>
                                     </select>
                                 </div>
