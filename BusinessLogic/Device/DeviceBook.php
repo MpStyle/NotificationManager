2938c9f78f6faa2f89f982e94889ad8522088e0f
Michele Pagnin <pagnin.michele@gmail.com>
2015-11-14 11:42:47 +0100

    Improved PHPDoc for DeviceBook.

--- BusinessLogic/Device/DeviceBook.php
+++ BusinessLogic/Device/DeviceBook.php
@@ -30,6 +30,19 @@ use MToolkit\Model\Sql\MPDOResult;
 final class DeviceBook
 {
 
+    /**
+     * Returns a MList of Device filtered by parameters.
+     * 
+     * @param int $id
+     * @param int $enabled
+     * @param int $applicationId
+     * @param string $type
+     * @param string $freeSearch
+     * @param int $mobileId
+     * @param int $perPage
+     * @param int $page
+     * @return MList
+     */
     public static function getDevices( $id = null, $enabled = null, $applicationId = null, $type = null, $freeSearch = null, $mobileId = null, $perPage = 10000000, $page = 0 )
     {
         MDataType::mustBeNullableInt( $id );
@@ -44,62 +57,35 @@ final class DeviceBook
         /* @var $deviceList MPDOResult */ $deviceList = DeviceAction::get( $id, $enabled, $applicationId, $type, $freeSearch, $mobileId, $perPage, $page );
         /* @var $devices MList */ $devices = new MList();
 
-        if( $deviceList != null )
+        if( $deviceList==null )
         {
-            foreach( $deviceList as $currentDevice )
+            return $devices;
+        }
+        
+        foreach( $deviceList as $currentDevice )
+        {
+
+            $device = new Device();
+
+            foreach( $currentDevice as $key => $value )
             {
+                $codeKey = lcfirst( $key );
 
-                $device = new Device();
-
-                foreach( $currentDevice as $key => $value )
-                {
-                    $codeKey = lcfirst( $key );
-
-                    $device->$codeKey = $value;
-                }
-
-                $devices->append( $device );
+                $device->$codeKey = $value;
             }
+
+            $devices->append( $device );
         }
 
         return $devices;
     }
-//
-//    public static function getNotification( $deviceId, $applicationId )
-//    {
-//        MDataType::mustBeInt( $deviceId );
-//        MDataType::mustBeInt( $applicationId );
-//
-//        /* @var $notificationList MPDOResult */ $notificationList = DeviceAction::getNotification( $deviceId, $applicationId );
-//        /* @var $notifications MList */ $notifications = new MList();
-//
-//        if( $notificationList != null )
-//        {
-//            foreach( $notificationList as $currentNotification )
-//            {
-//
-//                $notification = new Notification();
-//
-//                foreach( $currentNotification as $key => $value )
-//                {
-//                    $codeKey = lcfirst( $key );
-//
-//                    $notification->$codeKey = $value;
-//                }
-//
-//                $notifications->append( $notification );
-//            }
-//        }
-//
-//        return $notifications;
-//    }
 
     public static function getDeviceType()
     {
         /* @var $typeList MPDOResult */ $typeList = DeviceAction::getType();
         /* @var $types MList */ $types = new MList();
 
-        if( $typeList != null )
+        if( $typeList!=null )
         {
             foreach( $typeList as $currentType )
             {
