f0afb98d16afc30f6d0bc011559d7d5d1d0a7ca4
Michele Pagnin <pagnin.michele@gmail.com>
2015-11-14 11:42:17 +0100

    Added Pagination class.

---
+++ Web/Views/Pagination.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Web\Views;
+
+use MToolkit\Controller\MAbstractViewController;
+
+class Pagination extends MAbstractViewController
+{
+    private $range = 5;
+    private $pageCount;
+
+    /**
+     * The constructor requires the total page count.
+     * 
+     * @param int $pageCount
+     * @param MAbstractViewController $parent
+     */
+    public function __construct( $pageCount, MAbstractViewController $parent = null )
+    {
+        parent::__construct( __DIR__ . '/Pagination.view.php', $parent );
+        $this->pageCount = $pageCount;
+    }
+
+    public function getStartFrom()
+    {
+        $startFrom = $this->getCurrentPage()-$this->range;
+        if( $startFrom<0 )
+        {
+            $startFrom = 0;
+        }
+
+        return $startFrom;
+    }
+
+    public function getEndTo()
+    {
+        $endTo = $this->getCurrentPage()+$this->range;
+        if( $endTo>$this->pageCount )
+        {
+            $endTo = $this->pageCount;
+        }
+
+        return $endTo;
+    }
+
+    public function getQueryString()
+    {
+        $queryString = $_SERVER['QUERY_STRING'];
+        if( \MToolkit\Core\MString::isNullOrEmpty( $queryString ) )
+        {
+            return "";
+        }
+
+        return $queryString . "&";
+    }
+
+    public function getCurrentPage()
+    {
+        $currentPage = $this->getGet()->getValue( "page" )==null ? 0 : (int) $this->getGet()->getValue( "page" );
+        return $currentPage;
+    }
+
+    public function showFirst()
+    {
+        if( $this->getStartFrom()==0 )
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function showPrevious()
+    {
+        if( $this->getCurrentPage()<=0 )  
+        {
+            return false;
+        }
+        
+        return true;
+    }
+
+    public function showNext()
+    {
+        if( $this->getCurrentPage()>=$this->pageCount-1 )  
+        {
+            return false;
+        }
+        
+        return true;
+    }
+
+    public function showLast()
+    {
+        if( $this->getEndTo()==$this->pageCount )
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returne the total page count
+     * 
+     * @return int
+     */
+    public function getPageCount()
+    {
+        return $this->pageCount;
+    }
+
+}
---
+++ Web/Views/Pagination.view.php
@@ -0,0 +1,44 @@
+<?php
+/* @var $this Web\Views\Pagination */
+?>
+<ul class="pagination">
+    <?php if( $this->showFirst() ): ?>
+        <li class="first" title="First">
+            <a href="?<?php $this->getQueryString() ?>page=0">
+                First
+            </a>
+        </li>
+    <?php endif; ?>
+
+    <?php if( $this->showPrevious() ): ?>
+        <li class="previous" title="Previous">
+            <a href="?<?php $this->getQueryString() ?>page=<?php echo $this->getCurrentPage()-1 ?>">
+                &laquo;
+            </a>
+        </li>
+    <?php endif; ?>
+
+    <?php for( $i = $this->getStartFrom(); $i<$this->getEndTo(); $i++ ): ?>
+        <li class="<?php echo ($this->getCurrentPage()==$i ? "active" : "") ?>">
+            <a href="?<?php $this->getQueryString() ?>page=<?php echo $i ?>">
+                <?php echo $i+1 ?>
+            </a>
+        </li>
+    <?php endfor; ?>
+
+    <?php if( $this->showNext() ): ?>
+        <li class="next" title="Next">
+            <a href="?<?php $this->getQueryString() ?>page=<?php echo $this->getCurrentPage()+1 ?>">
+                &raquo;
+            </a>
+        </li>
+    <?php endif; ?>
+
+    <?php if( $this->showLast() ): ?>
+        <li class="last" title="Last">
+            <a href="?<?php $this->getQueryString() ?>page=<?php echo $this->getPageCount()-1 ?>">
+                Last
+            </a>
+        </li>
+    <?php endif; ?>
+</ul>
